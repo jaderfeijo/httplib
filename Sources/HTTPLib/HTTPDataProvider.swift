d246619613750c09cb193ef8f8f1f4b3863c5a9d
Jader Feijo <jader.feijo@gmail.com>
2022-06-28 16:58:07 +0100

    Implement HTTPDataProvider tests

--- Sources/HTTPLib/HTTPDataProvider.swift
+++ Sources/HTTPLib/HTTPDataProvider.swift
@@ -7,15 +7,16 @@ import FoundationNetworking
 
 /// HTTP Data Provider.
 ///
-/// This class provides data via HTTP to it's consumers.
-public class HTTPDataProvider: DataProvider {
-
-	private let session: URLSession
+/// This class provides data via HTTP to its consumers.
+public class HTTPDataProvider {
+	let session: URLSession
 
 	public init(session: URLSession = .shared) {
 		self.session = session
 	}
+}
 
+extension HTTPDataProvider: DataProvider {
 	public func send(_ request: DataProviderRequest, callback: @escaping DataProviderClosure) throws {
 		let httpRequest = try URLRequest.from(
 			request: request)
@@ -33,33 +34,9 @@ public class HTTPDataProvider: DataProvider {
 	}
 }
 
-// MARK: - Private -
+// MARK: - Internal -
 
-private extension HTTPDataProvider {
-	func upload(_ request: URLRequest, body: Data, callback: @escaping DataProviderClosure) {
-		session.uploadTask(with: request, from: body) { data, response, error in
-			callback(
-				.mappedResponse(
-					from: data,
-					response: response,
-					error: error)
-			)
-		}.resume()
-	}
-
-	func download(_ request: URLRequest, callback: @escaping DataProviderClosure) {
-		session.dataTask(with: request) { data, response, error in
-			callback(
-				.mappedResponse(
-					from: data,
-					response: response,
-					error: error)
-			)
-		}.resume()
-	}
-}
-
-private extension DataProviderResponse {
+extension DataProviderResponse {
 	static func mappedResponse(from data: Data?, response: URLResponse?, error: Error?) -> Self {
 		guard error == nil else {
 			return .unreachable
@@ -81,12 +58,12 @@ private extension DataProviderResponse {
 	}
 }
 
-private extension URLRequest {
-	static func from(request: DataProviderRequest) throws -> URLRequest {
-		struct InvalidURLError: Swift.Error {
-			let url: String
-		}
+extension URLRequest {
+	struct InvalidURLError: Swift.Error {
+		let url: String
+	}
 
+	static func from(request: DataProviderRequest) throws -> URLRequest {
 		guard let url = URL(string: request.url) else {
 			throw InvalidURLError(url: request.url)
 		}
@@ -94,6 +71,33 @@ private extension URLRequest {
 		let urlRequest = NSMutableURLRequest(url: url)
 		urlRequest.httpMethod = request.method.rawValue
 		urlRequest.allHTTPHeaderFields = request.headers
+		urlRequest.httpBody = request.body
 		return urlRequest as URLRequest
+	}
+}
+
+// MARK: - Private -
+
+private extension HTTPDataProvider {
+	func upload(_ request: URLRequest, body: Data, callback respondWith: @escaping DataProviderClosure) {
+		session.uploadTask(with: request, from: body) { data, response, error in
+			respondWith(
+				.mappedResponse(
+					from: data,
+					response: response,
+					error: error)
+			)
+		}.resume()
+	}
+
+	func download(_ request: URLRequest, callback respondWith: @escaping DataProviderClosure) {
+		session.dataTask(with: request) { data, response, error in
+			respondWith(
+				.mappedResponse(
+					from: data,
+					response: response,
+					error: error)
+			)
+		}.resume()
 	}
 }
