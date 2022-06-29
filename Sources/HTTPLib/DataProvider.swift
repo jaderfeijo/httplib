5f5fae59e8038d83bbd0abc975eb13625b94606c
Jader Feijo <jader.feijo@gmail.com>
2022-06-29 11:41:02 +0100

    Add public initialiser to DataProviderRequest

--- Sources/HTTPLib/DataProvider.swift
+++ Sources/HTTPLib/DataProvider.swift
@@ -14,6 +14,17 @@ public struct DataProviderRequest {
 	public let url: String
 	public let headers: [String: String]?
 	public let body: Data?
+
+	public init(
+		method: HTTPMethod,
+		url: String,
+		headers: [String: String]? = nil,
+		body: Data? = nil) {
+		self.method = method
+		self.url = url
+		self.headers = headers
+		self.body = body
+	}
 }
 
 /// Data Provider Response.
