1a30e2f2347229bb23c3719065b8acf92e068990
Jader Feijo <jader.feijo@gmail.com>
2023-05-04 19:29:38 +0100

    Add documentation to EncodableDataProviderRequest initialiser

--- Sources/HTTPLib/DataProvider/Request/EncodableDataProviderRequest.swift
+++ Sources/HTTPLib/DataProvider/Request/EncodableDataProviderRequest.swift
@@ -8,6 +8,20 @@ public struct EncodableDataProviderRequest<T: Encodable>: DataProviderRequest {
 	public let headers: [String: String]
 	public let body: Data?
 
+	/**
+	 Initializes a new instance of `DataProviderRequest` with the given parameters.
+
+	 - Parameters:
+	   - method: The HTTP method used for the request.
+	   - url: The URL of the endpoint where the data is being requested from.
+	   - headers: Additional headers to be sent with the request.
+	   - body: The body of the request.
+	   - encoder: The JSON encoder to use to encode the request body. This parameter is optional and defaults to a new instance of `JSONEncoder`.
+
+	 - Throws: An error if the provided `body` parameter cannot be encoded by the `JSONEncoder`.
+
+	 - Note: This initializer encodes the `body` parameter using the provided `encoder`, and sets the resulting `Data` object as the value for the `body` property.
+	 */
 	public init(
 		method: HTTPMethod,
 		url: String,
