823319de8a7d7e32ce7ac4dd68969301707ca54e
Jader Feijo <jader.feijo@gmail.com>
2023-05-23 16:22:56 +0100

    Remove unnecessary explicit equality

--- Sources/HTTPLib/DataProvider/Request/DataProviderRequest.swift
+++ Sources/HTTPLib/DataProvider/Request/DataProviderRequest.swift
@@ -8,25 +8,3 @@ public protocol DataProviderRequest: Equatable {
 	var headers: [String: String] { get }
 	var body: Data? { get }
 }
-
-// MARK: - Equality -
-
-extension DataProviderRequest {
-	public static func == (left: any DataProviderRequest, right: any DataProviderRequest) -> Bool {
-		let equalHeaders = (left.headers == right.headers)
-
-		var equalBody = false
-		if let leftBody = left.body, let rightBody = right.body {
-			equalBody = (leftBody == rightBody)
-		} else if left.body == nil && right.body == nil {
-			equalBody = true
-		}
-
-		return (
-			left.method == right.method &&
-				left.url == right.url &&
-				equalHeaders &&
-				equalBody
-		)
-	}
-}
